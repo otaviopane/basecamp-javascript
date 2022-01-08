0d53e731eb714c953fc7a96f5bedbea188acd1c7
Otavio Pane <otavio.pane@znc.com.br>
2022-01-07 21:49:25 -0300

    Created a sum numbers check

---
+++ zMyLearning/checkNumbersSum/script.js
@@ -0,0 +1,54 @@
+function check(n1, n2) {
+  alert("Checking Numbers!");
+  n1 = Number(prompt("Type the first number: "));
+  n2 = Number(prompt("Type the second number: "));
+
+  itsEqual();
+  sumBetween10and20();
+
+  function itsEqual() {
+    if (n1 === n2) {
+      alert(`These ${n1} & ${n2} numbers are equal.`);
+    } else {
+      alert(`These ${n1} & ${n2} numbers are not equal.`);
+    }
+  }
+
+  function sumBetween10and20() {
+    let sum = n1 + n2;
+
+    alert(`The sum is ${sum}.`);
+
+    if (sum > 10 && sum < 20) {
+      alert(`It's between 10 & 20.`);
+    } else if (sum < 10) {
+      alert(`It's less than 10.`);
+    } else if (sum === 10) {
+      alert(`It's equal 10.`);
+    } else if (sum > 20) {
+      alert(`It's more than 20.`);
+    } else if (sum === 20) {
+      alert(`It's equal 20.`);
+    }
+  }
+  newCheck();
+}
+function newCheck() {
+  let choice = Number(
+    prompt(
+      `Type your choice's number:
+          \n1 - New check
+          \n2 - That's it`
+    )
+  );
+  switch (choice) {
+    case 1:
+      check();
+      break;
+    case 1:
+      alert("That's all folks");
+      break;
+  }
+}
+
+check();
